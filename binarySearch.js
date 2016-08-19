ee74e4d866da03419a2bdb32613f259071786fda
Sameer Rastogi <srastogi@srastogi-mac.hsd1.ca.comcast.net>
2016-08-19 07:55:10 -0700

    section b and c

--- binarySearch.js
+++ binarySearch.js
@@ -1,8 +1,10 @@
 
 /** Returns the array index of the element if found else -1
 */
+
 'use strict';
-function seachBinary(a, arr) {
+
+function iterativeBinarySearch(a, arr) {
     var min = 0;
     var max = arr.length - 1;
     var i;
@@ -20,9 +22,22 @@ function seachBinary(a, arr) {
             max = i - 1;
         }
         else if (val == a) {
-            return i;
+            return true;
         }
     }
 
-    return -1;
+    return false;
+}
+
+function recursiveBinarySearch(a, arr, min, max) {
+  if(!min) min=0;
+  if(!max) max = arr.length-1;
+
+  if (min > max) return false;
+     var i = Math.floor((min + max)/2);
+     if (arr[i] == a) return true;
+     else if (arr[i] < a)
+        return recursiveBinarySearch(a, arr, i+1, max);
+     else
+        return recursiveBinarySearch(a, arr, min, i-1);
 }
